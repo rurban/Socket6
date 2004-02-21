e64f423758dc76d49a37f01d3aa8825a1e8ac578
Hajimu Umemoto <ume@mahoroba.org>
2004-02-20 21:25:43 -0600

    import Socket6 0.16 from CPAN
    git-cpan-module:   Socket6
    git-cpan-version:  0.16
    git-cpan-authorid: UMEMOTO
    git-cpan-file:     authors/id/U/UM/UMEMOTO/Socket6-0.16.tar.gz

--- Socket6.pm
+++ Socket6.pm
@@ -31,12 +31,12 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-# $Id: Socket6.pm,v 1.28 2004/02/21 03:10:55 ume Exp $
+# $Id: Socket6.pm,v 1.29 2004/02/21 03:21:16 ume Exp $
 
 package Socket6;
 
 use vars qw($VERSION @ISA @EXPORT %EXPORT_TAGS);
-$VERSION = "0.15";
+$VERSION = "0.16";
 
 =head1 NAME
 
